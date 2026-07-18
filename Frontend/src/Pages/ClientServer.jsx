f903fea50fab938b3c9da4bdf3f10fa215475ee3
Prince kumar <princechouhan754288@gmail.com>
2026-07-18 17:03:49 +0530

    complete project

--- Frontend/src/Pages/ClientServer.jsx
+++ Frontend/src/Pages/ClientServer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import axios from "axios";
 
 const ClientServer = axios.create({
-  // baseURL: "https://scan-app-production-6816.up.railway.app/",
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://scan-app-production-6816.up.railway.app/",
+  // baseURL: "http://localhost:3000/",
   withCredentials: true,
 });
 
