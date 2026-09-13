d3bf013f897597a350425ec5e3220a0662456fd5
prince kumar <225900373+princekumar9234@users.noreply.github.com>
2026-09-13 12:38:25 +0530

    Update Navbar.jsx

--- Frontend/src/Pages/Navbar.jsx
+++ Frontend/src/Pages/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
       <div className="flex justify-between border-2 m-3 mt-3 rounded-2xl border-neutral-800 bg-[#0c0f16]/90 backdrop-blur-md shadow-[0_8px_20px_rgba(0,0,0,0.6)] items-center py-4 px-6">
         {/* Logo */}
         <div className="font-bold text-2xl flex items-center whitespace-nowrap gap-2">
-          <span>Scan</span> <span className="text-yellow-600">App</span>
+          <span>Food</span> <span className="text-yellow-600">factz</span>
         </div>
 
         {/* Desktop Menu */}
